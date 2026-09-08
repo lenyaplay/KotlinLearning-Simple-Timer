36c07a49285d836013b61c65f9eb3d19d0edfb79
lenyaplay <lenyaplay@mail.ru>
2026-09-09 01:22:00 +0300

    feat: add fallback for none exact timer

--- app/src/main/java/com/lenyaplay/simple/timer/data/SystemAlarmScheduler.kt
+++ app/src/main/java/com/lenyaplay/simple/timer/data/SystemAlarmScheduler.kt
@@ -4,6 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.SystemClock
 import com.lenyaplay.simple.timer.AlarmConstants
 import com.lenyaplay.simple.timer.receivers.TimerReceiver
@@ -20,11 +21,22 @@ internal class SystemAlarmScheduler(private val context: Context) : AlarmSchedul
 
     override fun schedule(afterMs: Long) {
         val triggerAtMillis = SystemClock.elapsedRealtime() + afterMs
-
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtMillis, pendingIntent
-        )
+
+        val canScheduleExact =
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.S || alarmManager.canScheduleExactAlarms()
+        if (canScheduleExact) {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtMillis, pendingIntent,
+            )
+        } else {
+            // Fallback на неточный таймер, если не получилось поставить точный
+            alarmManager.set(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtMillis, pendingIntent
+            )
+        }
+
+
     }
 
     override fun cancel() {
